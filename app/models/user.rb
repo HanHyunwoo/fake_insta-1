453ee68ecb965a2d3d0b80373fd7376e8438da9b
classtak <classtak@gmail.com>
2018-07-10 04:18:49 +0000

    fb login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,27 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :likes
   has_many :liked_posts, through: :likes, source: :post
+  # User.find_auth
+  def self.find_auth(auth)
+    # Identity가 있는지?
+    # identity에 uid랑 provider 정보가 일치하는 게 없으면,
+    # 새로 만든다. => user는 nil이다.
+    # 만약에 일치한다면, user_id가 있을 거니까 user 오브젝트가 리턴
+    identity = Identity.find_auth(auth)
+    user = identity.user
+    # user가 있는지?
+    if user.nil?
+      user = User.new(
+        email: auth.info.email,
+        name: auth.info.name,
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user.save!
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    user
+  end
 end
