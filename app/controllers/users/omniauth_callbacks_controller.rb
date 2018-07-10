02b342d5dce522c9373d9b02173ac3499311b242
classtak <classtak@gmail.com>
2018-07-10 05:02:47 +0000

    fb login end

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,8 +7,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     p "********************************"
     p request.env['omniauth.auth']
     p "********************************"
+    #<OmniAuth::AuthHash credentials=
+    #<OmniAuth::AuthHash expires=true expires_at=1536372457 token="*********">
+    #extra=#<OmniAuth::AuthHash raw_info=#<OmniAuth::AuthHash email="romancedandelion@gmail.com" id="***********" name="Takhee Kim">>
+    #info=#<OmniAuth::AuthHash::InfoHash
+    #   email="romancedandelion@gmail.com"
+    #   image="http://graph.facebook.com/v2.10/1735946999835206/picture"
+    #   name="Takhee Kim">
+    # provider="facebook"
+    # uid="************">
     auth = env['omniauth.auth']
-    @user = User.find_auth(auth)
+    @user = User.find_auth(auth, current_user)
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
     else
